5af9a1a3000bebdfef4efc0f1eec20a048c13df2
Ritesh kumar sah <riteshkumar555sah@gmail.com>
2024-04-14 20:02:37 +0530

    Changes in FooterCom...

--- src/Components/FooterCom.jsx
+++ src/Components/FooterCom.jsx
@@ -8,7 +8,7 @@ const FooterCom = () => {
         </div>
         <div className="flex justify-center cursor-pointer">
           <Link
-            href="/contact"
+            to="/contact"
             className="mr-6 text-black-600 dark:text-black-200"
           >
             <svg
@@ -21,7 +21,7 @@ const FooterCom = () => {
             </svg>
           </Link>
           <Link
-            href="/contact"
+            to="/contact"
             className="mr-6 text-black-600 dark:text-black-200"
           >
             <svg
@@ -34,7 +34,7 @@ const FooterCom = () => {
             </svg>
           </Link>
           <Link
-            href="/contact"
+            to="/contact"
             className="mr-6 text-black-600 dark:text-black-200"
           >
             <svg
@@ -46,7 +46,7 @@ const FooterCom = () => {
               <path d="M7 11v2.4h3.97c-.16 1.029-1.2 3.02-3.97 3.02-2.39 0-4.34-1.979-4.34-4.42 0-2.44 1.95-4.42 4.34-4.42 1.36 0 2.27.58 2.79 1.08l1.9-1.83c-1.22-1.14-2.8-1.83-4.69-1.83-3.87 0-7 3.13-7 7s3.13 7 7 7c4.04 0 6.721-2.84 6.721-6.84 0-.46-.051-.81-.111-1.16h-6.61zm0 0 17 2h-3v3h-2v-3h-3v-2h3v-3h2v3h3v2z" />
             </svg>
           </Link>
-          <Link href="/contact" className="text-black-600 dark:text-black-200">
+          <Link to="/contact" className="text-black-600 dark:text-black-200">
             <svg
               xmlns="http://www.w3.org/2000/svg"
               className="h-4 w-4"
@@ -189,7 +189,7 @@ const FooterCom = () => {
       <div className="border-t border-neutral-200 dark:border-neutral-500 py-4">
         <p className="text-sm text-black-500 dark:text-black-400 text-center md:text-left ">
           © 2024 Your Company. All rights reserved.&nbsp;&nbsp;&nbsp;
-          <Link href="/policy" className="underline">
+          <Link to="/policy" className="underline">
             Privacy
           </Link>
         </p>

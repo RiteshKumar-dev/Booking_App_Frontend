59aeb340f808a28806a08296fc1b9e3f2788bc9d
Ritesh kumar sah <riteshkumar555sah@gmail.com>
2024-05-09 14:13:33 +0530

    Same changes in Auth_Components...

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   build: {
     rollupOptions: {
-      external: ['react-google-login'],
+      external: ['../react-google-login'],
     },
   },
 });
